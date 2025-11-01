8c1e157aa3d8d92357b48fb9f6beb5662084ab18
zerocode0806 <ubaidillahdahlan@gmail.com>
2025-11-01 16:17:31 +0700

    update kalkulator bangun

--- playground/kalkulator-bangun.py
+++ playground/kalkulator-bangun.py
@@ -1,27 +1,27 @@
 # pilih opsi perhitungan
-print("a. keliling lingkaran")
-print("b. luas lingkaran")
-print("c. luas segitiga")
-print("d. keliling segitiga")
+print("1. keliling lingkaran")
+print("2. luas lingkaran")
+print("3. luas segitiga")
+print("4. keliling segitiga")
 print(" ")
 
-operasi = input("Masukkan opsi perhitungan a, b, c, d : ")
+operasi = input("Pilih opsi perhitungan 1, 2, 3, 4 : ")
 
 # lakukan perhitungan berdasarkan opsi yang dipilih
-if operasi == "a" :
+if operasi == "1" :
     r = float(input("Masukkan jari jari lingkaran (r) : "))
     print(f"Keliling dari lingkran dengan jari jari {r} adalah {2 * 3.14 * r}")
-elif operasi == "b" :
+elif operasi == "2" :
     r = float(input("Masukkan jari jari lingkaran (r) : "))
     print(f"Keliling dari lingkran dengan jari jari {r} adalah {3.14 * r * r}")
-elif operasi == "c" :
+elif operasi == "3" :
     a = float(input("Masukkan alas (a) : "))
     t = float(input("Masukkan tinggi (t) : "))
     print(f"Luas dari segitiga dengan alas {a} dan tinggi {t} adalah {0.2 * a * t}")
-elif operasi == "d" :
-    x = float(input("Masukkan panjang sisi 1 : "))
-    y = float(input("Masukkan panjang sisi 2 : "))
-    z = float(input("Masukkan panjang sisi 3 : "))
+elif operasi == "4" :
+    x = float(input("Masukkan panjang sisi a : "))
+    y = float(input("Masukkan panjang sisi b : "))
+    z = float(input("Masukkan panjang sisi c : "))
     print(f"Keliling dari segitiga dengan panjang sisi a {x}, sisi b {y}, dan sisi c {z} adalah {x + y + z}")
 else : 
-    print("pilihan tidak valid, pilih a, b, c, atau d")
+    print("pilihan tidak valid, pilih antara 1, 2, 3, atau 4")
