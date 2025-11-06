6218d3612a44686593355f052406ddb65fa1bcb4
zerocode0806 <ubaidillahdahlan@gmail.com>
2025-11-06 11:54:01 +0700

    adjust program seleksi pekerja

--- playground/sistem-seleksi-pelamar.py
+++ playground/sistem-seleksi-pelamar.py
@@ -15,23 +15,20 @@ while ulang.lower() == 'y':
     umur = int(input("Masukkan umur pelamar: "))
     pendidikan = str(input("Msukkan pendidikan terakhir anda (SMA/D3/S1/S2): "))
 
-    # ======== cek parameter usia ========# 
     if umur < 21 :
-        print("Tidak di terima, masih terlalu muda") 
+        print("Ditolak, masih terlalu muda") 
         total_ditolak += 1
     elif umur > 27 :
-        print("Tidak di terima, terlalu tua ")
+        print("Ditolak, terlalu tua ")
         total_ditolak += 1
-    # ====================================#
     
-    # ===== cek parameter pendidikan =====#
-    elif umur >= 21 and pendidikan == "S1" or pendidikan == "S2" :
-        print("diterima, sudah memenuhi syarat")
-        total_diterima += 1
     else :
-        print("Tidak di terima, tidak memenuhi syarat")
-        total_ditolak += 1
-    # ====================================#
+        if pendidikan == "S1" or pendidikan == "S2" :
+            print("Diterima, sudah memenuhi syarat")
+            total_diterima += 1
+        else :
+            print("Ditolak, tidak memenuhi syarat")
+            total_ditolak += 1
     
     ulang = input("Input data lagi? (y/t): ")
     
